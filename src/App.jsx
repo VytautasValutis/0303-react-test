b1a09b963922b26a617e931315ea9a470e2f9fff
Vytautas Valutis <dnc908@gmail.com>
2023-03-02 16:01:18 +0200

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
