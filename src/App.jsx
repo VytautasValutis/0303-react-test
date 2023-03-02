0a39351bbc2b47dd7364c82ebebb4dde7d06259d
Vytautas Valutis <dnc908@gmail.com>
2023-03-02 16:06:18 +0200

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
+  console.log('Hello world!');
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>Hello world!</h1>
       </header>
     </div>
   );
