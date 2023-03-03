8b95181655783467f010f7a576fbbdbdd66d591b
Vytautas Valutis <dnc908@gmail.com>
2023-03-03 15:18:43 +0200

    May by fin

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
       .then(res => {
         console.log(res.data);
-        setData(res.data.jokes);
+        setData(res.data.jokes.map((u, i) => ({ ...u, row: i })));
       });
   }, []);
 
@@ -26,10 +26,19 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>Joke List</h1>
-        <ul>
+        <ul className="list-group">
           {
-            data?.map(u => <li key={u.id}><p>{u.setup + " "}</p>
-              <small>{u.delivery}</small>
+            data?.map(u => u.type === "single" ?
+            <li className="list-group-item"
+            style={{backgroundColor: u.row % 2 === 0 ? "gray" : "darkgray"}} 
+            key={u.id}>
+            <p>{u.joke}</p>
+            </li> 
+            :
+            <li className="list-group-item"
+            style={{backgroundColor: u.row % 2 === 0 ? "gray" : "darkgray"}}
+            key={u.id}><p>{u.setup}</p>
+              <p>{u.delivery}</p>
             </li>)
           }
         </ul>
